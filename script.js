1697f5b3ea8cd724d2b9f12c3d81bce339fd1b3d
flo-314 <julianflo1916@gmail.com>
2021-10-01 18:43:54 -0300

    Diseño y print library arreglado

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ const addBookContainer = document.querySelector(".addbook-container");
 const addBtn = document.querySelector(".bookBtn");
 const inputContainers = document.querySelectorAll(".input");
 const closeBtn = document.querySelector(".bookBtnX");
-const sumbitBtn = document.querySelector(".sumbitBtn")
-const titleInput = document.querySelector(".inputTitle")
-const autorInput = document.querySelector(".inputAutor")
-const pagesInput = document.querySelector(".inputPages")
-const readStateInput = document.querySelector(".inputReadState")
-
-
+const sumbitBtn = document.querySelector(".sumbitBtn");
+const titleInput = document.querySelector(".inputTitle");
+const autorInput = document.querySelector(".inputAutor");
+const pagesInput = document.querySelector(".inputPages");
+const readStateInput = document.querySelector(".inputReadState");
 
 //toma los valores de los input y les pasa la funcion addbook cuando es clickeado
 sumbitBtn.addEventListener("mousedown", () => {
-    addBookToLibrary(titleInput.value, autorInput.value, pagesInput.value, readStateInput.checked)
-}) 
+  addBookToLibrary(
+    titleInput.value,
+    autorInput.value,
+    pagesInput.value,
+    readStateInput.checked
+  );
+});
 // saca el hidden
 addBtn.addEventListener("mousedown", () => {
   addBookContainer.hidden = false;
@@ -46,32 +49,30 @@ function Book(title, autor, pages, readState) {
 
 //crea un nevo objeto usando como prototipo la funcion book
 function addBookToLibrary(title, autor, pages, readState) {
-    //Tansforma el True/False de la checkbox en una string
-    if(readState == true){
-        readState = "read it"
-    }
-    else{
-        readState = "didn't read it"
-    }
-    let add = new Book(title, autor, pages, readState);
+  //Tansforma el True/False de la checkbox en una string
+  if (readState == true) {
+    readState = true
+  } else {
+    readState =  false
+  }
+  printBook(title, autor, pages, readState)
+  let add = new Book(title, autor, pages, readState);
   myLibrary.push(add);
 }
-
-
-// printea los books como cotainers.
-function printBooks() {
-  myLibrary.forEach((element) => {
-    let e = document.createElement("div");
+function printBook(title, autor, pages, readState){
+ let e = document.createElement("div");
+    let h3 = document.createElement("h3")
+    let h4 = document.createElement("h4")
+    let h5 = document.createElement("h5")
+    let h6 = document.createElement("h6")
+    let readBox = document.createElement("input")
+    readBox.type = "checkbox"
+    readBox.checked = readStateInput.checked
     e.className = "book";
-    e.textContent =
-      element.title +
-      " of " +
-      element.autor +
-      " it have's " +
-      element.pages +
-      " and you " +
-      element.readState;
+    h3.textContent = title
+    h4.textContent = autor
+    h5.textContent = pages
+    h6.textContent = readState
     bookContainer.appendChild(e);
-  });
+    e.append(h3,h4,h5,h6,readBox)
 }
-
